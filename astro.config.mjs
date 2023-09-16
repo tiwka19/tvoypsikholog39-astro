194bba624208e183fb9a0f5a65ea8d7c1bcbe03b
somethingxdev <hevnxv19@gmail.com>
2023-09-16 12:50:27 +0200

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {  assets: true },
-  integrations: [tailwind()]
+  integrations: [tailwind({ applyBaseStyles: false})],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,24 +1,31 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: ['class', '[data-theme="dark"]'],
-	theme: {
-		screens: {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: ['class', '[data-theme="dark"]'],
+  theme: {
+    screens: {
       sm: '576px',
       md: '768px',
       lg: '976px',
       xl: '1306px',
     },
-		container: {
-			center: true,
-		},
-		extend: {
-			fontFamily: {
-				body: ['Raleway'],
-				sans: ['El Messiri Variable', ...defaultTheme.fontFamily.sans],
-			},
-		},
-	},
-	plugins: [],
-}
+    container: {
+      center: true,
+    },
+
+    extend: {
+      colors: {
+        primary: '#8AD9A6',
+        base: '#1B1B1E',
+        gray: '#636363',
+        white: '#fff',
+      },
+      fontFamily: {
+        body: ['Raleway'],
+        sans: ['El Messiri Variable', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
